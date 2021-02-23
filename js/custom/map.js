e3783ffa8d47d27d0d6d2c896bb3c4bc7d604fae
ilpise <pisewip@yahoo.it>
2021-02-23 14:48:21 +0100

    Added feature info - TO BE COMPLETED

--- js/custom/map.js
+++ js/custom/map.js
@@ -10,6 +10,8 @@
     attach: function (context, settings) {
       // console.log('Drupal.behaviors.OlMap');
 
+      Drupal.behaviors.OlMap.FEATURE_COUNT = Drupal.behaviors.OlMap.FEATURE_COUNT || 10;
+
       // @ layerStrategy : 1 == vengono costruite source ol.source.ImageWMS
       //                        inserite in layer lo.layer.Image
       //                      new ol.layer.Image({
@@ -110,6 +112,7 @@
               var t = new ol.layer.Image({
                 // visible: true,
                 // visible: false,
+                  type: 'layer',
                   title: val.Title,
                   source: new ol.source.ImageWMS({
                     url: qgsUrl + '?map=' + qgsMap,
@@ -191,6 +194,187 @@
                   layerStrategy: layerStrategy });
 
           $('#layers > ul > li.layer-switcher-fold > button').trigger('click')	;
+
+
+          // Change mouse pointer when on map and the info-tab is selected
+          olMap.getViewport().addEventListener('mouseover', function(evt){
+              // console.info('in');
+              // console.log($('#info-tab').attr("class"));
+              if ($('#info-tab').hasClass("active")){
+                $(this).css( 'cursor', 'crosshair' );
+              } else {
+                $(this).css( 'cursor', 'default' );
+              }
+          }, false);
+
+          // Feature info behavior
+          olMap.on('singleclick', function(evt) {
+              // console.log(evt);
+              console.log('singleclick');
+              var li = '';
+              var tabPane = '';
+              var viewResolution = /** @type {number} */ (view.getResolution());
+
+              olMap.forEachLayerAtPixel(evt.pixel, function(layer) {
+                  // console.log(evt.pixel);
+                  // console.log(layer.get('type'));
+                  // console.log(layer.getKeys());
+                  // console.log(layer.getProperties());
+                  // var id = layer.get('id');
+                  // console.log(id);
+                  // var title = layer.get('title');
+                  // console.log('Title');
+                  // console.log(title);
+
+                  // Add tematismo only for layers (exclude base layers)
+                  // console.log(layer.getProperties());
+                  // console.log(layer.getProperties().type);
+                  if (layer.getProperties().type !== 'base'){
+                    console.log('type layer');
+                    wmsSource = layer.getSource();
+
+                    // console.log(wmsSource.ol_uid);
+                    // console.log(wmsSource.getKeys());
+                    // console.log(wmsSource.getProperties());
+                    //	https://docs.qgis.org/3.4/en/docs/user_manual/working_with_ogc/server/services.html#getfeatureinfo
+
+                      var url = wmsSource.getFeatureInfoUrl(
+                        evt.coordinate, viewResolution, 'EPSG:3857',
+                        {
+                          // 'INFO_FORMAT': 'text/html'
+                          // 'INFO_FORMAT': 'text/plain'
+                          'INFO_FORMAT': 'text/xml',
+                          // 'INFO_FORMAT': 'application/json'
+                          'FI_POINT_TOLERANCE': 16,
+                          'FI_LINE_TOLERANCE': 8,
+                          'FI_POLYGON_TOLERANCE': 4,
+                          'FEATURE_COUNT': Drupal.behaviors.OlMap.FEATURE_COUNT
+                        });
+                      if (url) {
+                        // console.log(url);
+                        fetch(url)
+                          .then(function (response) {
+                            // console.log(response);
+                            return response.text();
+                          })
+                          .then(function (xml) {
+                            // console.log(xml);
+                            parser = new DOMParser();
+                            // xmlDoc = parser.parseFromString(xml, "text/xml");
+                            var xmlDoc = $.parseXML( xml );
+                            var xml = $( xmlDoc );
+                            var layer = xml.find( "Layer" );
+                            var feature = xml.find( "Feature" );
+
+                            // console.log(layer);
+                            console.log(feature.length);
+                            if(feature.length > 0){
+                              // console.log(wmsSource.ol_uid);
+                              var title = xml.find( "Layer" ).attr('name');
+                              var machineName = title.replace(/\s/g, '');
+                              // var html;
+                              // console.log(title);
+                              // console.log('FEATURE');
+                              // console.log(feature);
+
+                              // var panel_group = '<div class="panel-group">';
+                              var table = '';
+                              // var accord = '';
+                              xml.find("Feature").each(function (key) {
+                                // console.log(key);
+                                // console.log('Feature');
+                                // console.log($(this));
+                                // console.log($(this).attr('id'));
+                                // console.log($(this).get(0));
+                                // console.log($(this).context);
+
+                                if (key == 0) {
+                                  var infirst = 'in';
+                                } else {
+                                  var infirst = '';
+                                }
+
+                                var featureId = "feature-"+$(this).attr('id');
+
+                                table += '<div class="card">'+
+                                            '<div class="card-header">'+
+                                              '<h6 class="panel-title">'+
+                                                '<a data-toggle="collapse" data-parent="#infocontent-'+machineName+'" href="#'+featureId+'">'+featureId+'</a>'+
+                                              '</h6>'+
+                                            '</div>'+
+                                            '<div id="'+featureId+'" class="panel-collapse collapse '+infirst+'" style="max-height: 40vh; overflow: auto;">'+
+                                '<table class="table table-striped">';
+                                $(this).find("Attribute").each(function () {
+                                  // console.log($(this).attr('name'));
+                                  // console.log($(this).attr('value'));
+                                  if($(this).attr('name').toLowerCase() == 'foto'){
+                                    table += '<tr><td><b>'+$(this).attr('name')+'</b></td><td><img class="img-responsive open-img" src="'+$(this).attr('value')+'"/></td></tr>';
+                                  } else if ($(this).attr('name').toLowerCase() == 'link'){
+                                    table += '<tr><td><b>'+$(this).attr('name')+'</b></td><td><a href="'+$(this).attr('value')+'">'+$(this).attr('name')+'</a></td></tr>';
+                                  } else {
+                                    table += '<tr><td><b>'+$(this).attr('name')+'</b></td><td>'+$(this).attr('value')+'</td></tr>';
+                                  }
+                                });
+                                table += '</table></div></div>';
+
+                              });
+
+                              // var panel_group = '<div class="panel-group">'+table+'</div>';
+
+                                li += '<li>'+
+                                          // '<input id="opt-'+title+'" name="intervaltype" checked type="radio" data-target="'+title+'" data-toggle="tab">'+
+                                          // '<label for="optDaily">Daily</label>'+
+                                          '<div class="radio">' +
+                                          '<label data-target="#infotab-'+machineName+'"><input type="radio" name="radiolayer">'+title+'</label>' +
+                                          '</div>' +
+                                      '</li>';
+                                tabPane += '<div class="tab-pane" id="infotab-'+machineName+'">'+
+                                                // '<p>'+title+'</p>'+
+                                                '<div class="panel-group" id="infocontent-'+machineName+'">'+table+'</div>'+
+                                                // '<div id="infocontent-'+title+'">'+panel_group+'</div>'+
+                                            '</div>';
+
+
+                            }
+
+                            var content = '<h5>Layers interrogati:</h5><ul id="info-list" class="nav nav-tabs nav-info" style="display:inline-grid;">';
+                                content += li;
+                                content += '</ul>';
+                                // content += '<hr/>'
+                                content += '<h5>Informazioni:</h5><div class="tab-content ">';
+                                content += tabPane;
+                                content += '</div>';
+
+                            // Reset info element
+                            document.getElementById('info').innerHTML = '';
+
+                            document.getElementById('info').innerHTML = content;
+
+                            $('input[name="radiolayer"]').click(function () {
+                                //jQuery handles UI toggling correctly when we apply "data-target" attributes and call .tab('show')
+                                //on the <li> elements' immediate children, e.g the <label> elements:
+                                console.log('radio clicked');
+                                console.log($(this).closest('label').attr("data-target"));
+                                // $(this).closest('label').trigger('click');
+                                $(this).closest('label').tab('show');
+                                // $(this).closest('label').attr("data-target").tab('show');
+                            });
+
+                            $('#info-list > li > div > label > input').first().trigger('click');
+
+                            // $('img.open-img').click(function (){
+                            //   var img = '<img class="img-responsive" style="margin:auto;" src="'+$(this).attr('src')+'"/>'
+                            //   $('#imagecontainer').html(img);
+                            //   $('#imgTitle').html();
+                            //
+                            //   $('#imageModal').modal('show');
+                            // });
+
+                          });
+                      }
+                  }
+              });
+            });
 
         }
         /**/
